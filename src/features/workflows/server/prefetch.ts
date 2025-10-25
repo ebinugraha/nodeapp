ad7e84ae5b1dbe1840d98f5282c0f0a47cf8defe
FebrianNugraha <febrian.14nugraha@gmail.com>
2025-10-25 15:11:16 +0700

    feat 13-workflow-page

--- src/features/workflows/server/prefetch.ts
+++ src/features/workflows/server/prefetch.ts
@@ -10,3 +10,11 @@ type Input = inferInput<typeof trpc.workflows.getAll>;
 export const prefetchWorkflows = async (params: Input) => {
   return await prefetch(trpc.workflows.getAll.queryOptions(params));
 };
+
+export const prefetchWorkflow = async (id: string) => {
+  return await prefetch(
+    trpc.workflows.getOne.queryOptions({
+      id,
+    })
+  );
+};
