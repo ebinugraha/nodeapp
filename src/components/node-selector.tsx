8059fe4da046b1a6c5d88ae70bdd61d7689a14a2
FebrianNugraha <febrian.14nugraha@gmail.com>
2025-11-12 18:52:03 +0700

    feat: 23 stripe trigger

--- src/components/node-selector.tsx
+++ src/components/node-selector.tsx
@@ -38,6 +38,13 @@ const triggerNodes: NodeTypeOption[] = [
       "Runs the flow when a Google Form is submitted. Useful for collecting responses",
     icon: "/logos/googleform.svg",
   },
+  {
+    type: NodeType.STRIPE_TRIGGER,
+    label: "Stripe Trigger",
+    description:
+      "Runs the flow when a Stripe event is captured. Useful for payment processing",
+    icon: "/logos/stripe.svg",
+  },
 ];
 
 const executionNode: NodeTypeOption[] = [
---
+++ src/features/triggers/components/stripe-trigger/node.tsx
@@ -0,0 +1,40 @@
+import { NodeProps } from "@xyflow/react";
+import { memo, useState } from "react";
+import { BaseTriggerNode } from "../base-trigger-node";
+import { useNodeStatus } from "@/features/executions/hooks/use-node-status";
+import { googleFormTriggerChannel } from "@/inngest/channels/google-form-trigger";
+import { fetcStripeTriggerToken } from "./actions";
+import { StripeTriggerDialog } from "./dialog";
+import { stripeTriggerChannel } from "@/inngest/channels/stripe-trigger";
+
+export const StripeTriggerNode = memo((props: NodeProps) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleSetting = () => {
+    setIsDialogOpen(true);
+  };
+
+  const nodeStatus = useNodeStatus({
+    nodeId: props.id,
+    channel: stripeTriggerChannel().name,
+    topic: "status",
+    refreshToken: fetcStripeTriggerToken,
+  });
+
+  return (
+    <>
+      <StripeTriggerDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
+      <BaseTriggerNode
+        {...props}
+        Icon={"/logos/stripe.svg"}
+        status={nodeStatus}
+        name="Stripe'"
+        description="when stripe event is captured"
+        onSettings={handleSetting}
+        onDoubleClick={handleSetting}
+      />
+    </>
+  );
+});
+
+StripeTriggerNode.displayName = "StripeTriggerNode";
